2f8ab95b27f7b51afed5d1a4a0d0dada1a6ed5e2
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-03-25 01:14:55 +0900

    Add byebug

--- lib/amazon_auth.rb
+++ lib/amazon_auth.rb
@@ -3,6 +3,7 @@ Dotenv.load
 require "active_support/all"
 require "capybara"
 require "highline/import"
+require 'byebug'
 require_relative "amazon_auth/version"
 require_relative "amazon_auth/amazon_info"
 require_relative "amazon_auth/converter"
