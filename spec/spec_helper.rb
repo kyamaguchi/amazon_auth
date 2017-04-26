147174ca2e211a5b40c46207bd849a568d8e2509
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-04-26 17:07:50 +0900

    Enable rspec options with status

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'amazon_auth'
 
 Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+end
