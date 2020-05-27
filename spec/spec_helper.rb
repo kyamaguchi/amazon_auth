a4bc309807fbb64c8b182ccda528842ff2e1238a
kyamaguchi <kzh.yap@gmail.com>
2020-05-28 01:33:58 +0900

    Auto-correct with Lint/NonDeterministicRequireOrder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'amazon_auth'
 
-Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**/*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**/*.rb")].sort.each {|f| require f}
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
